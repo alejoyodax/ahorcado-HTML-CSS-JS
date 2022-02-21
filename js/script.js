0f98dfb324c4790094094f07ad1e1123c8fb7698
Alejandro Silva <alejoyodax@gmail.com>
2022-02-21 08:50:53 -0500

    cambios menores

--- js/script.js
+++ js/script.js
@@ -1,47 +1,72 @@
 //Variables GLOBALES
-var LETRAS = [] // Contiene las letras que deben ser adivinadas ej: ["J","I","R","A","F","A"]
-var LETRASadiv = [] // Contiene las letras adivinadas hasta el momento ej: ["J","__","__","A","__","A"]
-var WORDS = ['CONTENIDO', 'JOYA', 'DETONADOR', 'INCESANTE', 'NIÑO', 'REPTIL', 'DESORIENTACIÓN', 'OMNÍVORO', 'MENTAL', 'ASEQUIBLE', 'GUERRERO', 'MODA', 'DESLEAL', 'INMUNDICIA', 'RESCATE', 'CIFRADO', 'BENEVOLENTE', 'PIE', 'DEFORME', 'MANDO', 'GRANERO', 'ABSTINENTE', 'CÉNTRICO', 'AMABLE', 'ORUGA', 'PÉLVICO', 'DESMEMBRAMIENTO', 'SERENIDAD', 'DESAGÜE', 'JOROBADO', 'ERÓTICA', 'FEDERAL', 'PADRE', 'CHILLÓN', 'INÚTIL', 'GHETTO', 'CERO', 'PERRO', 'TANQUE', 'PICNIC', 'SOÑOLIENTO', 'ENCANTADOR', 'MALEFICIO', 'HABLAR', 'BALLET', 'SUBSÓNICO', 'HIENA', 'CARICIA', 'DIRIGIBLE', 'ELEVACIÓN', 'CARACOL', 'HAZME', 'HIPOPÓTAMO', 'DIABÓLICO', 'EVIDENCIA', 'LUJURIA', 'FANTÁSTICO', 'CABALLERO', 'DIFUMINAR', 'BURRO', 'CASA', 'CLUB', 'VERDAD', 'PODER', 'GENTE', 'ATRAPAMOSCAS', 'OESTE', 'JARDINES', 'RELUCIENTE', 'ATRAPAMOSCAS', 'SÚPER', 'POESÍA', 'IMÁN', 'CORTO', 'MESTIZO', 'ACTITUD', 'BOLSO', 'ADIVINAR', 'LEVANTAMIENTO', 'PROFÉTICO', 'SANITARIO', 'DEMONIO', 'NECRÓFAGO', 'DESINTEGRACIÓN', 'CHIFLADO', 'VAGABUNDO', 'GOBERNADOR', 'ENTRADA', 'ANIMALISTA', 'PERFORAR', 'ESPALDA', 'PROVEEDOR', 'ENREDADERA', 'CHANTAJE', 'MINIPÍLDORA', 'AGRESOR', 'FUERZA', 'POSITIVO', 'EXTRAVAGANTE', 'MÁXIMO', 'DRAMÁTICO', 'PROYECTO', 'CREADOR', 'ESPOSA']
-var WORD = ""
-var INTENTOS = 5
+var LETRAS = [], // Contiene las letras que deben ser adivinadas ej: ["J","I","R","A","F","A"]
+    LETRASadiv = [], // Contiene las letras adivinadas hasta el momento ej: ["J","__","__","A","__","A"]
+    WORDS = ['CONTENIDO', 'JOYA', 'DETONADOR', 'INCESANTE', 'NIÑO', 'REPTIL', 'DESORIENTACIÓN', 'OMNÍVORO', 'MENTAL', 'ASEQUIBLE', 'GUERRERO', 'MODA', 'DESLEAL', 'INMUNDICIA', 'RESCATE', 'CIFRADO', 'BENEVOLENTE', 'PIE', 'DEFORME', 'MANDO', 'GRANERO', 'ABSTINENTE', 'CÉNTRICO', 'AMABLE', 'ORUGA', 'PÉLVICO', 'DESMEMBRAMIENTO', 'SERENIDAD', 'DESAGÜE', 'JOROBADO', 'ERÓTICA', 'FEDERAL', 'PADRE', 'CHILLÓN', 'INÚTIL', 'GHETTO', 'CERO', 'PERRO', 'TANQUE', 'PICNIC', 'SOÑOLIENTO', 'ENCANTADOR', 'MALEFICIO', 'HABLAR', 'BALLET', 'SUBSÓNICO', 'HIENA', 'CARICIA', 'DIRIGIBLE', 'ELEVACIÓN', 'CARACOL', 'HAZME', 'HIPOPÓTAMO', 'DIABÓLICO', 'EVIDENCIA', 'LUJURIA', 'FANTÁSTICO', 'CABALLERO', 'DIFUMINAR', 'BURRO', 'CASA', 'CLUB', 'VERDAD', 'PODER', 'GENTE', 'ATRAPAMOSCAS', 'OESTE', 'JARDINES', 'RELUCIENTE', 'ATRAPAMOSCAS', 'SÚPER', 'POESÍA', 'IMÁN', 'CORTO', 'MESTIZO', 'ACTITUD', 'BOLSO', 'ADIVINAR', 'LEVANTAMIENTO', 'PROFÉTICO', 'SANITARIO', 'DEMONIO', 'NECRÓFAGO', 'DESINTEGRACIÓN', 'CHIFLADO', 'VAGABUNDO', 'GOBERNADOR', 'ENTRADA', 'ANIMALISTA', 'PERFORAR', 'ESPALDA', 'PROVEEDOR', 'ENREDADERA', 'CHANTAJE', 'MINIPÍLDORA', 'AGRESOR', 'FUERZA', 'POSITIVO', 'EXTRAVAGANTE', 'MÁXIMO', 'DRAMÁTICO', 'PROYECTO', 'CREADOR', 'ESPOSA'],
+    WORD = "",
+    intentosRestantes = 5
 
-const init = function() {
+const init = function () {
+    // selecciona una palabra al azar del array WORDS
     WORD = WORDS[Math.floor(Math.random() * WORDS.length)]
+    // llenamos los dos array previamente declarados para contener la letras
     for (let i = 0; i < WORD.length; i++) {
         LETRAS.push(WORD[i])
-        LETRASadiv.push('___')
+        LETRASadiv.push('__')
     }
     document.getElementById("estado-palabra").textContent = LETRASadiv.join(" ")
-    document.getElementById("intentos-restantes").textContent = INTENTOS
+    document.getElementById("intentos-restantes").textContent = intentosRestantes
 }
 
-const findReplaceLetter = function(letter) {
-    isInWord = false
-    for (let i = 0; i < LETRAS.length; i++) {
-        if (LETRAS[i] === letter) {
-            LETRASadiv[i] = letter;
-            isInWord = true
+const verificarLetra = function (letra) {
+    function sinTilde(letra) {
+        switch (letra) {
+            case "Á":
+                return 'A'
+            case "É":
+                return "E"
+            case "Í":
+                return "I"
+            case "Ó":
+                return "O"
+            case "Ú":
+                return "U"
+        }
+        return letra
+    }
+
+
+    siApareceEnPalabra = false
+    let longitud = LETRAS.length
+    for (let i = 0; i < longitud; i++) {
+        if (sinTilde(LETRAS[i]) === letra) {
+            // si la letra se encuentra en la palabra
+            LETRASadiv[i] = LETRAS[i];
+            siApareceEnPalabra = true;
         }
     }
-    if (!isInWord) {
-        INTENTOS = INTENTOS - 1
-        console.log(INTENTOS)
-        document.getElementById("intentos-restantes").textContent = INTENTOS
+
+    if (!siApareceEnPalabra) {
+        intentosRestantes -= 1
+        console.log(intentosRestantes)
+        document.getElementById("intentos-restantes").textContent = intentosRestantes
     }
 }
 
-const pulsarBoton = function() {
+const pulsarBoton = function () {
     document.getElementById("estado-palabra").textContent = LETRASadiv.join(" ")
     var x = this.textContent
-    findReplaceLetter(x)
+    verificarLetra(x)
     document.getElementById("estado-palabra").textContent = LETRASadiv.join(" ")
     this.style.backgroundColor = "gray";
+    this.state = 0;
+
+
 }
 
-const RecargarPagina = function() {
-        location.reload()
-    }
-    // Agregamos funcion a llas teclas
+const RecargarPagina = function () {
+    location.reload()
+}
+// Agregamos funcion a las teclas
 const botones = document.querySelectorAll(".tecla");
 // Recorremos el array botones
 botones.forEach(boton => {
